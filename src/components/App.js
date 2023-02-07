c69e89ae9c7a45870a45cc6321b8a87727563a97
Vishal Vaish <114350565+vishal-vaish@users.noreply.github.com>
2023-02-07 11:01:36 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { useReducer } from 'react'
 import { todoReducer } from '../reducers/todo';
-import '../styles/App.css';
+import '../styles.css';
 import { AddTodo } from './AddTodo';
 import { Todo } from './Todo';
 const App = () => {
-  const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
+  // const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
+  const [state,dispatch] = useReducer(todoReducer,[]);
   
- 
   return (
     <div id="main">
-      <AddTodo />
+      <AddTodo dispatch={dispatch} />
+      {state.map(todo=>{
+        return <Todo title={todo.title} key={todo.id} dispatch={dispatch} id={todo.id}/>
+      })}
      {/* Render list of Todo Components here */}
     </div>
   )
